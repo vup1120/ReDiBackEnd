5661d99eec88d4b8a6c6477d737b9f62ff4f28be
vup1120 <vup1120@gmail.com>
2020-09-14 17:50:36 +0200

    modify the code to be more efficient

--- homework_02_string.py
+++ homework_02_string.py
@@ -12,21 +12,14 @@ def split_consecutive_duplicates(givenlist):
     for i in range(len(givenlist)):
         if i == 0:
             tmp = [givenlist[i]]
+            split.append(tmp)
         elif givenlist[i] != givenlist[i-1]:
             tmp = [givenlist[i]]
+            split.append(tmp)
         elif givenlist[i] == givenlist[i-1]:
             tmp.append(givenlist[i])
-        split.append(tmp)
-    final = []
-    # Remove the duplicated sublists from the split list.
-    for i in range(len(split)):
-        if i == 0:
-            final.append(split[i])
-        elif split[i] != split[i-1]:
-            final.append(split[i])
-        elif split[i] == split[i-1]:  # skip if it equals to the previous sublist
-            continue
-    return final
+
+    return split
 
 
 input_list = [0, 0, 1,1,1, 2, 3, 4, 4, 6, 6, 6, 8, 4, 4]
